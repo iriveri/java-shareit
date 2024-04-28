72663fdd2bcc55e36f62b08b3cf0998391640109
iRiveri <rivers-property@yandex.ru>
2024-04-28 12:51:19 +0300

    feat: Logging on UserController is added

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -6,9 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-/**
- * TODO Sprint add-controllers.
- */
 @RestController
 @RequestMapping(path = "/users")
 @Slf4j
@@ -20,24 +17,27 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto newUser){
-      var user = service.addNewUser(newUser);
-      log.info("New user is created by id {}",1);
-      return ResponseEntity.status(HttpStatus.CREATED).body(user);
-    };
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto newUser) {
+        var user = service.addNewUser(newUser);
+        log.info("New user is created with ID {}", 1);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
     @PatchMapping("/{userId}")
     public ResponseEntity<UserDto> editUser(@PathVariable Long userId, @RequestBody UserDto userDto) {
         var user = service.editUser(userId , userDto);
+        log.info("User data for ID {} has been successfully patched",userId);
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
     @GetMapping("/{userId}")
     public ResponseEntity<UserDto> getUser(@PathVariable Long userId) {
         var user = service.getUser(userId);
+        log.info("User data for ID {} has been successfully extracted",userId);
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
     @DeleteMapping("/{userId}")
     public ResponseEntity<UserDto> deleteUser(@PathVariable Long userId) {
         var user = service.deleteUser(userId);
+        log.info("User with ID {} has been successfully deleted",userId);
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
