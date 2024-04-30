626acd87f90d4021c27593c3f2b8e01582360c6e
iRiveri <rivers-property@yandex.ru>
2024-04-30 13:45:43 +0300

    fix:check commit

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,13 +1,11 @@
 package ru.practicum.shareit.item.dto;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
