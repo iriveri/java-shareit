3651f89da29ff1127e9f89d4ee7b54b1dabce98d
iRiveri <rivers-property@yandex.ru>
2024-06-02 15:29:02 +0300

    feat: mapper and dto created

---
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
@@ -0,0 +1,15 @@
+package ru.practicum.shareit.request.service;
+
+import ru.practicum.shareit.request.model.ItemRequest;
+
+import java.util.List;
+
+public interface ItemRequestService {
+    ItemRequest createRequest(Long userId, ItemRequest requestDto);
+
+    List<ItemRequest> getUserRequests(Long userId);
+
+    List<ItemRequest> getAllRequests(Long userId, int from, int size);
+
+    ItemRequest getRequestById(Long userId, Long requestId);
+}
