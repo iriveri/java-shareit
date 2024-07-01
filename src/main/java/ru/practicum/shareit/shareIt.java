73acb38c7abc30ef1b02e02406837e7071fb4552
iRiveri <rivers-property@yandex.ru>
2024-07-01 23:52:41 +0300

    fix: codestyle fix

--- src/main/java/ru/practicum/shareit/shareIt.java
+++ src/main/java/ru/practicum/shareit/shareIt.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class shareit {
+public class shareIt {
 
     public static void main(String[] args) {
 
